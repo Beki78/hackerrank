021aa562e60930023896a44da1c0477ddb250eff
Baki78 <bakiget78@gmail.com>
2024-08-04 17:19:20 +0300

    1st

---
+++ arthemetic-operations.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    add = a + b
+    dif = a - b
+    pro = a * b
+    print(add)
+    print(dif)
+    print(pro)
+    
